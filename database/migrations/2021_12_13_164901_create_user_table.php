3cd3297f28e760d8832faaf122eb4b0a9b03b702
zhirobyte <faizhanafi90@gmail.com>
2021-12-14 00:28:25 +0700

    test update login

--- database/migrations/2021_12_13_164901_create_user_table.php
+++ database/migrations/2021_12_13_164901_create_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
+            $table->string('password');
+            $table->string('google_id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('nama_pembeli');
+            $table->string('alamat');
+            $table->string('no_telp');
+            $table->date('tgl_lahir');
+            $table->string('pekerjaan');;
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +37,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user');
     }
 }
